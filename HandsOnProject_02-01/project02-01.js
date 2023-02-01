12aff7fb2810393df2029583940b545274b6f740
Eli G <122303653+DargonBread@users.noreply.github.com>
2023-02-01 10:33:54 -0600

    Add files via upload

---
+++ HandsOnProject_02-01/project02-01.js
@@ -0,0 +1,32 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: Eli Galloway
+      Date:   
+
+      Filename: project02-01.js
+ */
+function FahrenheitToCelsius(degree)
+{
+      let total = (degree - 32) / 1.8;
+      return total;
+      
+}
+
+function CelsiusToFahrenheit(degree) 
+{
+      let total = (degree * 1.8) + 32;
+      return total;
+}
+
+document.getElementById("cValue").onchange = function() {
+      let cDegree = document.getElementById("cValue").value;
+      document.getElementById("fValue").value = CelsiusToFahrenheit(cDegree);
+}
+
+document.getElementById("fValue").onchange = function() {
+      let fDegree = document.getElementById("fValue").value;
+      document.getElementById("cValue").value = FahrenheitToCelsius(fDegree);
+}
